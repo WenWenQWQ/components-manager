28ae8031900bc4c326f9b69813dc933a8c2fded0
WenWenQWQ <1982528223@qq.com>
2019-01-06 16:52:52 +0800

    修复头部天气问题

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React,{ Component } from 'react';
+import './index.less'
 
 export default class Footer extends Component{
     render(){
         return (
-            <div>
-                This is a Footer
+            <div className="footer">
+                版权所有：WWW（推荐使用谷歌浏览器，可以获得更加操作页面体验），技术支持：WWW
             </div>
         )
     }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,22 +7,26 @@ export default class Header extends Component{
     componentWillMount(){
         this.setState({
             userName:'河畔一角'
-        })
+        });
         setInterval(()=>{
             let sysTime=Utils.formateDate(new Date().getTime());
             this.setState({
                 sysTime
             })
         },1000);
+        this.getWeatherAPIData();
     }
     getWeatherAPIData(){
         let city='北京';
         axios.jsonp({
-            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=L1zQtUpLDA5LMuAiM4naS4xzd2RyVuBZ'
         }).then((res)=>{
             if(res.status==='success'){
                 let data=res.results[0].weather_data[0];
-                this.setState
+                this.setState({
+                    dayPictureUrl:data.dayPictureUrl,
+                    weather:data.weather
+                })
             }
         })
     }
@@ -30,18 +34,23 @@ export default class Header extends Component{
         return (
             <div className="header">
                <Row className="header-top">
-                   <Col span="24">
+                   <Col span={ 24 }>
                        <span>欢迎，{this.state.userName}</span>
                        <a href="#">退出</a>
                    </Col>
                </Row>
                 <Row className="breadcrumb">
-                    <Col span="4" className="breadcrumb-title">
+                    <Col span={ 4 } className="breadcrumb-title">
                         首页
                      </Col>
-                    <Col span="20" className="weather">
+                    <Col span={ 20 } className="weather">
                         <span className="date">{this.state.sysTime}</span>
-                        <span className="weather-detail">晴转多云</span>
+                        <span className="weather-img">
+                            <img src={this.state.dayPictureUrl} alt=""/>
+                        </span>
+                        <span className="weather-detail">
+                             {this.state.weather}
+                        </span>
                     </Col>
                 </Row>
             </div>
